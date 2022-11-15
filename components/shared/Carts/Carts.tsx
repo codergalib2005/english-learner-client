03a55391dac4a696391cf9574bccef24e576f90c
codergalib20 <coderboygalif@gmail.com>
2022-11-16 00:21:50 +0600

    solve shipon vai card ideas

--- components/shared/Carts/Carts.tsx
+++ components/shared/Carts/Carts.tsx
@@ -8,14 +8,16 @@ export default function Carts() {
       {services.map((service) => (
         <div className="cart">
           <Link className="cart-link" href="/">
-            <div className="img-ring"></div>
-            <Image
-              className="cart-img"
-              src={service.img}
-              alt=""
-              width={100}
-              height={100}
-            />
+            <div className="img-ring">
+              <Image
+                className="cart-img"
+                src={service.img}
+                alt=""
+                width={100}
+                height={100}
+              />
+            </div>
+
             <h4>{service.name}</h4>
           </Link>
           <p>{service.topic}</p>
