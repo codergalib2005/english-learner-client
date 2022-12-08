cdf191c974cceeb8cdb89328089692e1c3a93dd4
minhazpro3 <minhazpro1@gmail.com>
2022-12-08 22:55:01 +0600

    blog ui set

--- components/shared/InformationSide/Blogs.tsx
+++ components/shared/InformationSide/Blogs.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TiStopwatch } from "react-icons/ti";
 
 const Blogs = () => {
   const blogs = [
@@ -28,13 +29,22 @@ const Blogs = () => {
     <div className="blog_component_informationSide">
       <span>Blogs</span>
       {blogs.map((blog) => (
-        <div key={blog.id} className="blog_component_sub_child_div">
-          <div className="blog_component_texts">
-            <h6>{blog.title}</h6>
-            <p>{blog.description}</p>
+        <div className="blog_component_blog_cart" key={blog.id}>
+          <div className="blog_component_sub_child_div">
+            <div className="blog_component_texts">
+              <h6>{blog.title}</h6>
+              <p>{blog.description}</p>
+            </div>
+            <div className="blog_component_image">
+              <img src={blog.image} />
+            </div>
           </div>
-          <div className="blog_component_image">
-            <img src={blog.image} />
+          <div className="blog_publish_time">
+            <p>
+              {" "}
+              <TiStopwatch />
+            </p>
+            <span>2 weeks ago</span>
           </div>
         </div>
       ))}
