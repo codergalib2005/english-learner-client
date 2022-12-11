bff7cca0be217ac02fbe95b5381be035ed8346b3
minhazpro3 <minhazpro1@gmail.com>
2022-12-11 22:37:30 +0600

    add banner

--- components/Games/Banner.tsx
+++ components/Games/Banner.tsx
@@ -4,19 +4,22 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="game_banner_component">
-      <div className="game_banner_img">
-        {/* <Image
-          src="https://i.ibb.co/x8J1x2D/game-banner.jpg"
-          alt="Picture of the author"
-          width={500}
-          height={500}
-        /> */}
-      </div>
-      <div className="game_banner_text_field">
-        <h3>
-          Talent wins games, but <br /> teamwork and intelligence <br /> wins
-          championships. <br /> ~ Michael Jordan
-        </h3>
+      <div className="game_banner_content">
+        <div className="game_banner_text_field">
+          <h3>
+            Talent wins games, but teamwork and intelligence <br /> wins
+            championships. <br /> ~ Michael Jordan
+          </h3>
+        </div>
+        <div className="game_banner_img">
+          <Image
+            className="banner_img"
+            src="https://i.ibb.co/tqLtr6R/hero-position-img.png"
+            alt="Picture of the author"
+            width={500}
+            height={400}
+          />
+        </div>
       </div>
     </div>
   );
--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../components/Games/Banner";
 import CardCarousel from "../../components/Games/CardCarousel";
+import Header from "../../components/shared/Header/Header";
 
 const Games = () => {
   return (
     <div>
+      <Header />
       <Banner />
       <CardCarousel />
     </div>
