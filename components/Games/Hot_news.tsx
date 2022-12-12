9136eb8a44d1399b66ac688f97f2514d3352cc84
minhazpro3 <minhazpro1@gmail.com>
2022-12-13 01:07:51 +0600

    news blog card

--- components/Games/Hot_news.tsx
+++ components/Games/Hot_news.tsx
@@ -5,19 +5,19 @@ const Hot_news = () => {
   const news = [
     {
       id: 1,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      title: "consectetur  adipisicing consectetur elit.",
       des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit ipsam laboriosam iure nulla commodi ut, dolorem a possimus, doloremque labore eligendi? ",
       img: "https://i.ibb.co/6WJFtTQ/3777183.jpg",
     },
     {
       id: 2,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      title: "consectetur  adipisicing consectetur elit.",
       des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit ipsam laboriosam iure nulla commodi ut, dolorem a possimus, doloremque labore eligendi? ",
       img: "https://i.ibb.co/6WJFtTQ/3777183.jpg",
     },
     {
       id: 3,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      title: "consectetur  adipisicing consectetur elit.",
       des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit ipsam laboriosam iure nulla commodi ut, dolorem a possimus, doloremque labore eligendi?",
       img: "https://i.ibb.co/6WJFtTQ/3777183.jpg",
     },
@@ -38,13 +38,13 @@ const Hot_news = () => {
           {news.map((single_news, index) => (
             <div className="news_card_child">
               <div className="news_card_content">
-                <div>
+                <div >
                   <Image
                     style={{ borderRadius: "5px 5px 0px 0px" }}
                     src={single_news.img}
                     alt=""
                     width={180}
-                    height={130}
+                    height={160}
                   />
                 </div>
                 <div className="news_text">
