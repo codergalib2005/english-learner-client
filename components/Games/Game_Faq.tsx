08a68d1c909770ad816ff26f873ed4c974390800
minhazpro3 <minhazpro1@gmail.com>
2022-12-12 22:43:44 +0600

    faq title style

--- components/Games/Game_Faq.tsx
+++ components/Games/Game_Faq.tsx
@@ -5,6 +5,9 @@ const Game_Faq = () => {
   const [active, setActive] = useState<string>("title1");
   return (
     <div className="game_faq_container">
+      <div>
+        <h3 className="faq_sec">FAQ!</h3>
+      </div>
       <Game_according_card />
       <Game_according_card />
       <Game_according_card />
