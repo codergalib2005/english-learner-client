a1e54ec200e53d7a5f51c74d6bbe8b6334de43c9
minhazpro3 <minhazpro1@gmail.com>
2022-12-18 01:01:38 +0600

    games top footer responsive

--- components/Games/Extra_footer.tsx
+++ components/Games/Extra_footer.tsx
@@ -3,18 +3,15 @@ import React from "react";
 
 const Extra_footer = () => {
   return (
-   <div className="extra_footer">
-     <div className="extra_footer_margin">
-      <div className="extra_img">
-        <Image src="https://i.ibb.co/yf4TcKz/img-earth.jpg" fill alt="" />
-        
-      </div>
+    <div className="extra_footer">
+      <div className="extra_footer_margin">
+        <div className="extra_img">
+          <Image src="https://i.ibb.co/yf4TcKz/img-earth.jpg" fill alt="" />
+        </div>
 
-      <p>
-        If you want to grow your knowledge. <br/> Play and Win!
-      </p>
+        <p>If you want to grow your knowledge. Play and Win!</p>
+      </div>
     </div>
-   </div>
   );
 };
 
